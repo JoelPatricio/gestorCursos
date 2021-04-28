d6e433b4d5642a9abc57c5651da386170886f1bd
JoelPatricio <joelhp19@outlook.es>
2021-04-28 01:05:52 -0500

    Terminado graficas promedio

--- graficaTodasUnidades.php
+++ graficaTodasUnidades.php
@@ -57,31 +57,27 @@
             </h2>
             <ul class="list-group list-group-flush">
               <li class="list-group-item py-2">
-                <!-- <span class="badge badge-warning">⚠</span>  -->
                 Las puntuaciones obtenidas en las coevaluaciones se evaluaron de la siguiente manera
                 <script src="https://cdn.jsdelivr.net/npm/chart.js@3.2.0/dist/chart.min.js"></script>
-                <canvas class="my-4 chartjs-render-monitor" id="myChart"width="332" height="129"
-                  style="display: block; height: 322px; width: 129px;"></canvas>
-<script>
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
+                <canvas class="my-4 chartjs-render-monitor" id="myChart"width="332" height="129"></canvas>
+                <script>
+                    var ctx = document.getElementById('myChart').getContext('2d');
+                    var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green'],
+        labels: ['Evaluaciones', 'Tareas', 'Asistencias'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5],
+            label: '% de alumnos',
+            data: [20, 30, 50],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(75, 192, 192, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(75, 192, 192, 1)'
             ],
             borderWidth: 1
         }]
