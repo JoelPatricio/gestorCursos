a1ab215a2e35e39f1c5449fd0b4f282aa46eb6e5
Joel Patricio <joelhp19@outlook.es>
2021-04-27 22:09:18 -0500

    Primer commit

---
+++ inicio.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="es_MX">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <!-- Custom styles -->
+  <link href="../assets/css/style.css" rel="stylesheet">
+
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+    crossorigin="anonymous"></script>
+
+  <!--login.js -->
+  <script src="../config/js/login.js"></script>
+
+  <title>Profesor</title>
+</head>
+
+<body class="container bg-light">
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      logo
+    </div>
+    <div class="col col-lg-4">
+      <div class="btn-group" role="group" aria-label="Basic example">
+        <button type="button" class="btn btn-primary">Incio</button>
+        <button type="button" class="btn btn-primary">Mis Cursos</button>
+        <button type="button" class="btn btn-primary">Cerrar Sesión</button>
+      </div>
+    </div>
+  </div>
+</div>
+<br><br><br><br><br>
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <div class="card col-8">
+        <div class="card-body">
+          <h5 class="card-title">Bienvenida</h5>
+            <p class="card-text text-justify">Bienvenido puede registrar la materia que imparte, registrar a sus alumnos y sus calificaciones, registra la rubrica con la que se les evalua.
+              <br>Con el objetivo de definir el rendimiento a cada uno de ellos . Se  le indicara que opciones tiene para mejorar el rendimiento dependiendo de los resultados.
+            </p>
+          <div class="text-right">
+          <a href="#" class="btn btn-primary">Registrar materia</a>
+        </div>
+      </div>
+    </div>
+    </div>
+    <div class="col col-lg-4">
+      <img src="resurce\Logo_de_la_BUAP.svg.png" class="img-fluid" alt="...">
+    </div>
+  </div>
+</div>
+
+
+
+
+</body>
+
+</html>
